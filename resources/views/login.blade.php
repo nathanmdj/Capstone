e2f8f0306b989269294eca261c55714cfbf5428e
Nikko Dawisan <dawisan.nikko@gmail.com>
2024-03-13 10:53:31 +0800

    Login - Nikko

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,63 @@
 @extends('layouts.login')
 @section('content')
-    <h1>LOGIN</h1>
-@endsection
+    <!---Main Container--->
+
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+
+        <!---Login Container--->
+
+        <div class="row border rounded-5 p-3 shadow box-area" style="background: #DEDEDE">
+
+            <!---Left Box--->
+
+            <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box"
+                style="background: #444444">
+                <div class="featured-image mb-3">
+                    <img src="images/devx.png" class="img-fluid" style="width: 250px;" alt="Left Icon">
+                </div>
+                <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">Be
+                    Verified</p>
+                <small class="text-white text-wrap text-center"
+                    style="width: 17rem; font-family: 'Courier New', Courier, monospace;">The Social Media for all Developers.</small>
+            </div>
+
+            <!---Right Box--->
+
+            <div class="col-md-6 right-box">
+                <div class="row align-items-center">
+                    <div class="header-text mb-4">
+                        <h2>Hello, Again</h2>
+                        <p>We're happy to have you back.</p>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control form-control-lg bg-light fs-6"
+                            placeholder="Email Address">
+                    </div>
+
+                    <div class="input-group mb-1">
+                        <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password">
+                    </div>
+
+                    <div class="input-group mb-5 d-flex justify-content-between">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="formCheck">
+                            <label for="formCheck" class="form-check-label text-secondary"><small>Remember
+                                    Me</small></label>
+                        </div>
+                        <div class="forgot">
+                            <small><a href='#'>Forgot Password?</a></small>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <button class="btn btn-lg btn-primary w-100 fs-6" style="background-color: #DA0037">Login</button>
+                    </div>
+                    <div class="input-group mb-3">
+                        <button class="btn btn-lg btn-light w-100 fs-6"><img src="images/google.png" style="width: 20px"
+                                class="me-2"><small>Sign In with Google</small></button>
+                    </div>
+                    <div class="row">
+                        <small>Don't have account? <a href="#">Sign Up</a></small>
+                    </div>
+                </div>
+            @endsection
