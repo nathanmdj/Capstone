daa21c3a3ca4b3ac8a11f6a287ca1f6c27d93971
nathanmdj <nathanmdj628@gmail.com>
2024-03-20 18:53:15 +0800

    fixed textarea autoResize

--- resources/views/include/post-update.blade.php
+++ resources/views/include/post-update.blade.php
@@ -4,7 +4,7 @@
         @method('PUT')
         <div class="text-area d-flex mb-3">
             <textarea name="content" id="content" class="form-control bg-primary text-info border-0 p-0 px-1"
-                style="resize: none; overflow-y: hidden;"oninput="autoAdjust(this)">{{ $post->content }}</textarea>
+                style="resize: none; overflow-y: hidden; "oninput="autoResize(this)">{{ $post->content }}</textarea>
         </div>
 
         <div class="post-btn d-flex justify-content-end px-3">
@@ -14,8 +14,13 @@
 </div>
 
 <script>
-    function autoAdjust(textarea) {
-        textarea.style.height = ''; // Reset the height to auto
-        textarea.style.height = textarea.scrollHeight + 'px'; // Set the height to match the content
+    function autoResize() {
+        var textarea = document.getElementById('content');
+        if (textarea) {
+            textarea.style.height = 'auto';
+            textarea.style.height = textarea.scrollHeight + 'px';
+        }
     }
+
+    document.addEventListener('DOMContentLoaded', autoResize);
 </script>
